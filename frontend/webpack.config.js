e9bbeb556f929c04dc4e6871c7fa861bd521d211
Yunseo Hwang <yunseo.dev@gmail.com>
2021-06-05 23:49:11 +0900

    feat: add GoogleAuth component

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,3 +1,5 @@
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
@@ -5,6 +7,9 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
+  plugins: [
+    new Dotenv({ path: './.env.development' }),
+  ],
   module: {
     rules: [
       { test: /\.tsx?$/, loader: 'ts-loader' },
