506a84f564f5acd9c06cc2615e09e7d18f7912da
namu <nameunskadms@hanmail.net>
2021-06-08 18:50:14 +0900

    add google login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import GoogleLogin from "react-google-login";
 
 export default function App() {
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
+
+  // const nodeEnv: string = process.env.REACT_APP_GOOGLE_KEY as string;
+  // console.log(nodeEnv);
+
   return (
-    <h1>woowahan learning</h1>
+    <div>
+      <GoogleLogin
+        clientId="1037965886564-vu3ot759mb426mkcbo492e6dpgbaha01.apps.googleusercontent.com"
+        // clientId={API_KEY}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+    </div>
   );
-};
+}
